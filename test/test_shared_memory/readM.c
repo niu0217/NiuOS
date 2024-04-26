23c18789a247d7b077c937e40fd438bb21f8378a
niu0217 <1033856427@qq.com>
2024-04-26 13:00:56 +0800

    实现共享内存

---
+++ test/test_shared_memory/readM.c
@@ -0,0 +1,39 @@
+/* ************************************************************************
+> File Name:     readM.c
+> Author:        niu0217
+> Created Time:  Fri 26 Apr 2024 10:49:59 AM CST
+> Description:   
+ ************************************************************************/
+
+#define __LIBRARY__
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/sched.h>
+#include <linux/kernel.h>
+
+_syscall1(int, shmat, int, shmid);
+_syscall2(int, shmget, unsigned int, key, size_t, size);
+
+#define BUFFER_SIZE 10 /* 缓冲区大小 */
+#define SHM_KEY 2018
+
+int main()
+{
+    int i;
+    int shm_id;
+    int *p = NULL;
+    if ((shm_id = shmget(SHM_KEY, BUFFER_SIZE * sizeof(int))) < 0)
+        printf("shmget failed!");
+
+    if ((p = (int *)shmat(shm_id)) < 0)
+        printf("shmat error!");
+
+    for (i = 0; i < BUFFER_SIZE; i++)
+    {
+        printf("%d\n", p[i]);
+    }
+}
+
